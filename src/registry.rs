ed284f28d48a98e143dc3611c93e2187985b8a21
Jonathan Nilsson <jonathan@voysys.se>
2020-11-22 22:19:07 +0100

    Inline

--- src/registry.rs
+++ src/registry.rs
@@ -1,5 +1,5 @@
 use super::ffi;
-use std::ffi::{c_void, CString};
+use std::ffi::c_void;
 
 pub trait RegistryApi {
     fn set(self, name: &[u8], api: *mut c_void, bytes: u32);
@@ -15,42 +15,52 @@ pub trait RegistryApi {
 }
 
 impl RegistryApi for *mut ffi::tm_api_registry_api {
+    #[inline]
     fn set(self, name: &[u8], api: *mut c_void, bytes: u32) {
         assert!(!self.is_null());
         unsafe { (*self).set.unwrap()(name.as_ptr() as _, api, bytes) }
     }
+    #[inline]
     fn remove(self, api: *mut c_void) {
         assert!(!self.is_null());
         unsafe { (*self).remove.unwrap()(api) }
     }
+    #[inline]
     fn get(self, name: &[u8]) -> *mut c_void {
         assert!(!self.is_null());
         unsafe { (*self).get.unwrap()(name.as_ptr() as _) }
     }
+    #[inline]
     fn get_optional(self, name: &[u8]) -> *mut c_void {
         assert!(!self.is_null());
         unsafe { (*self).get_optional.unwrap()(name.as_ptr() as _) }
     }
+    #[inline]
     fn add_implementation(self, name: &[u8], implementation: *mut c_void) {
         assert!(!self.is_null());
         unsafe { (*self).add_implementation.unwrap()(name.as_ptr() as _, implementation) }
     }
+    #[inline]
     fn remove_implementation(self, name: &[u8], implementation: *mut c_void) {
         assert!(!self.is_null());
         unsafe { (*self).remove_implementation.unwrap()(name.as_ptr() as _, implementation) }
     }
+    #[inline]
     fn implementations(self, name: &[u8], count: &mut u32) -> *mut *mut c_void {
         assert!(!self.is_null());
         unsafe { (*self).implementations.unwrap()(name.as_ptr() as _, count as _) }
     }
+    #[inline]
     fn add_listener(self, listener: *const ffi::tm_api_registry_listener_i) {
         assert!(!self.is_null());
         unsafe { (*self).add_listener.unwrap()(listener) }
     }
+    #[inline]
     fn static_variable(self, id: u64, size: u32, file: &[u8], line: u32) -> *mut c_void {
         assert!(!self.is_null());
         unsafe { (*self).static_variable.unwrap()(id, size, file.as_ptr() as _, line) }
     }
+    #[inline]
     fn log_missing_apis(self) {
         assert!(!self.is_null());
         unsafe { (*self).log_missing_apis.unwrap()() }
