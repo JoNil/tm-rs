055a014fc5290d02c6594e96babfa204697a9c7c
Jonathan Nilsson <jonathan@voysys.se>
2020-11-29 00:52:17 +0100

    Wrap update and filter

--- src/entity.rs
+++ src/entity.rs
@@ -1,5 +1,8 @@
 use crate::api::{Api, ApiWithCtx};
-use std::ffi::{c_void, CString};
+use std::{
+    ffi::{c_void, CString},
+    slice,
+};
 use tm_sys::ffi::{
     tm_component_i, tm_component_mask_t, tm_engine_i, tm_engine_o, tm_engine_update_set_t,
     tm_entity_api, tm_entity_context_o, TM_ENTITY_API_NAME,
@@ -39,25 +42,51 @@ impl ApiWithCtx for EntityApi {
     }
 }
 
-pub struct Engine<'a, C, E, W>
+pub struct Engine<'a, ComponentsIter, ExcludesIter, WritesIter, UpdateFn, FilterFn>
 where
-    C: IntoIterator<Item = &'a u32>,
-    E: IntoIterator<Item = &'a bool>,
-    W: IntoIterator<Item = &'a bool>,
+    ComponentsIter: IntoIterator<Item = &'a u32>,
+    ExcludesIter: IntoIterator<Item = &'a bool>,
+    WritesIter: IntoIterator<Item = &'a bool>,
+    UpdateFn: Fn(*mut tm_engine_update_set_t) + Send + Sync + 'static,
+    FilterFn: Fn(&[u32], &tm_component_mask_t) -> bool + Send + Sync + 'static,
 {
     pub name: &'static str,
     pub disabled: bool,
     pub num_components: u32,
-    pub components: C,
-    pub excludes: E,
-    pub writes: W,
-    pub update: unsafe extern "C" fn(inst: *mut tm_engine_o, data: *mut tm_engine_update_set_t),
-    pub filter: unsafe extern "C" fn(
-        inst: *mut tm_engine_o,
-        components: *const u32,
-        num_components: u32,
-        mask: *const tm_component_mask_t,
-    ) -> bool,
+    pub components: ComponentsIter,
+    pub excludes: ExcludesIter,
+    pub writes: WritesIter,
+    pub update: UpdateFn,
+    pub filter: Option<FilterFn>,
+}
+
+struct EngineCallbackData {
+    ctx: *mut tm_entity_context_o,
+    update: Box<dyn Fn(*mut tm_engine_update_set_t)>,
+    filter: Option<Box<dyn Fn(&[u32], &tm_component_mask_t) -> bool>>,
+}
+
+unsafe extern "C" fn engine_update(inst: *mut tm_engine_o, data: *mut tm_engine_update_set_t) {
+    let callback_data = (inst as *const EngineCallbackData).as_ref().unwrap();
+
+    (callback_data.update)(data);
+}
+
+unsafe extern "C" fn engine_filter(
+    inst: *mut tm_engine_o,
+    components: *const u32,
+    num_components: u32,
+    mask: *const tm_component_mask_t,
+) -> bool {
+    let callback_data = (inst as *const EngineCallbackData).as_ref().unwrap();
+    let components = slice::from_raw_parts(components, num_components as usize);
+    let mask = mask.as_ref().unwrap();
+
+    if let Some(filter) = &callback_data.filter {
+        filter(components, mask)
+    } else {
+        false
+    }
 }
 
 impl EntityApiInstance {
@@ -67,11 +96,15 @@ impl EntityApiInstance {
     }
 
     #[inline]
-    pub fn register_engine<'a, C, E, W>(&mut self, engine: Engine<'a, C, E, W>)
-    where
-        C: IntoIterator<Item = &'a u32>,
-        E: IntoIterator<Item = &'a bool>,
-        W: IntoIterator<Item = &'a bool>,
+    pub fn register_engine<'a, ComponentsIter, ExcludesIter, WritesIter, UpdateFn, FilterFn>(
+        &mut self,
+        engine: Engine<'a, ComponentsIter, ExcludesIter, WritesIter, UpdateFn, FilterFn>,
+    ) where
+        ComponentsIter: IntoIterator<Item = &'a u32>,
+        ExcludesIter: IntoIterator<Item = &'a bool>,
+        WritesIter: IntoIterator<Item = &'a bool>,
+        UpdateFn: Fn(*mut tm_engine_update_set_t) + Send + Sync + 'static,
+        FilterFn: Fn(&[u32], &tm_component_mask_t) -> bool + Send + Sync + 'static,
     {
         let name = CString::new(engine.name).unwrap();
 
@@ -90,6 +123,18 @@ impl EntityApiInstance {
             *out = *write;
         }
 
+        let has_filter_fn = engine.filter.is_some();
+
+        let inst = Box::into_raw(Box::new(EngineCallbackData {
+            ctx: self.ctx,
+            update: Box::new(engine.update),
+            filter: if let Some(filter) = engine.filter {
+                Some(Box::new(filter))
+            } else {
+                None
+            },
+        })) as *mut tm_engine_o;
+
         let engine = tm_engine_i {
             name: name.as_ptr(),
             disabled: engine.disabled,
@@ -98,9 +143,13 @@ impl EntityApiInstance {
             components,
             excludes,
             writes,
-            inst: self.ctx as *mut tm_engine_o,
-            update: Some(engine.update),
-            filter: Some(engine.filter),
+            inst,
+            update: Some(engine_update),
+            filter: if has_filter_fn {
+                Some(engine_filter)
+            } else {
+                None
+            },
         };
 
         unsafe {
@@ -115,6 +164,6 @@ impl EntityApiInstance {
 }
 
 #[inline]
-pub unsafe fn mask_has_component(mask: *const tm_component_mask_t, i: u32) -> bool {
-    ((*mask).bits[(i / 64) as usize] & (1 << (i % 64))) > 0
+pub fn mask_has_component(mask: &tm_component_mask_t, i: u32) -> bool {
+    (mask.bits[(i / 64) as usize] & (1 << (i % 64))) > 0
 }
