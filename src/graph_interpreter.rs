d8516a04cd2c80f71974414732b16d74b281b53a
Jonathan Nilsson <jonathan@voysys.se>
2020-12-01 22:50:48 +0100

    read_variable_f32

--- src/graph_interpreter.rs
+++ src/graph_interpreter.rs
@@ -1,5 +1,8 @@
-use crate::api::{Api, ApiWithCtx};
-use std::ffi::c_void;
+use crate::{
+    api::{Api, ApiWithCtx},
+    hash,
+};
+use std::{convert::TryInto, ffi::c_void, slice};
 use tm_sys::ffi::{
     tm_graph_interpreter_api, tm_graph_interpreter_o, tm_graph_interpreter_wire_content_t,
     TM_GRAPH_INTERPRETER_API_NAME,
@@ -39,7 +42,15 @@ impl ApiWithCtx for GraphInterpreterApi {
 }
 
 impl GraphInterpreterApiInstance {
-    pub fn read_variable(&mut self, variable: u64) -> tm_graph_interpreter_wire_content_t {
-        unsafe { (*self.api).read_variable.unwrap()(self.ctx, variable) }
+    pub fn read_variable_f32(&mut self, variable: &str) -> Option<f32> {
+        let variable = hash(variable.as_bytes());
+        let var = unsafe { (*self.api).read_variable.unwrap()(self.ctx, variable) };
+
+        if !var.data.is_null() {
+            let data = unsafe { slice::from_raw_parts(var.data as *mut u8, var.size as usize) };
+            Some(f32::from_ne_bytes(data.try_into().unwrap()))
+        } else {
+            None
+        }
     }
 }
